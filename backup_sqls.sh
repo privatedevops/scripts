5c9253528fd25477e9555333ba2d87c9d05d9a1a
Stanislav Stoyanov <stanislav@bgcode.com>
2017-03-15 15:46:35 +0200

    Changes in backup_sqls.sh, added option for arguments

--- backup_sqls.sh
+++ backup_sqls.sh
@@ -14,31 +14,35 @@ DAY30=`date -I -d "30 days ago"`
 #The source directory:
 BKPDIR="/backups/sqls/"
 SKIPFILES="/root/scripts/bkp_excludes.list"
-SRC="IP/HOST"
-SSHPORT='SSH PORT'
+
+SRC="127.0.0.1 127.0.0.2 127.0.0.3"
+
+SSHPORT='5698'
+
+if [[ $1 != '' ]]; then
+        SRC=$1
+fi
 
 CURDATE=`date  +"%d-%m-%Y-%Hh%Mm"`
 
-
-#Delete the backup from 30 days ago, if it exists
-if [ -d $BKPDIR/$server/$DAY30 ]; then
-	echo -e "Deleting backup from: $DAY30"
-	sudo rm -rf $BKPDIR/$server/$DAY30
-fi
 for server in $SRC; do
-
+        #Delete the backup from 30 days ago, if it exists
+        if [ -d $BKPDIR/$server/$DAY30 ]; then
+                echo -e "Deleting backup from: $DAY30"
+                sudo rm -rf $BKPDIR/$server/$DAY30
+        fi
         ssh -p $SSHPORT root@$server "mysqlcheck -Ar"
 
-	TRG="$BKPDIR/$server/$DAY0"
-	LNK="$BKPDIR/$server/$DAY1"
-	DBS=`ssh -p $SSHPORT root@$server 'mysql -e "show databases"' | grep -vE 'Database|schema'`	
-	if [ ! -d $TRG ]; then
-	        mkdir -p $TRG
-	fi
-	for db in $DBS; do
-		echo -e "Starting SQL backups for db $db from server: $server"
-	        ionice -c2 -n7 ssh -p $SSHPORT root@$server "mysqldump --events --skip-lock-tables $db" | /usr/bin/bzip2 > $TRG/$db-$CURDATE.bz2
-	done
+        TRG="$BKPDIR/$server/$DAY0"
+        LNK="$BKPDIR/$server/$DAY1"
+        DBS=`ssh -p $SSHPORT root@$server 'mysql -e "show databases"' | grep -vE 'Database|schema'`
+        if [ ! -d $TRG ]; then
+                mkdir -p $TRG
+        fi
+        for db in $DBS; do
+                echo -e "Starting SQL backups for db $db from server: $server"
+                ionice -c2 -n7 ssh -p $SSHPORT root@$server "mysqldump --events --skip-lock-tables $db" | /usr/bin/bzip2 > $TRG/$db-$CURDATE.bz2
+        done
 
-        ssh -p $SSHPORT root@$server "mysqlcheck -Ar"
+
 done
