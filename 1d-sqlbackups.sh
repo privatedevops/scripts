7cc0bbdaefb9d1a73a4e0ebf4a85d92f2b5d80b1
Stanislav Stoyanov <stanislav@bgcode.com>
2017-05-24 16:27:22 +0300

    added 1 day sql backups script

---
+++ 1d-sqlbackups.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+EXCLUDE="'Database|Database|information\_schema|performance\_schema|Database'"
+DESTINATION=/root/sql/
+CURDATE=`date  +"%d-%m-%Y-%Hh%Mm"`
+
+
+if [ ! -d $DESTINATION ]; then
+	mkdir $DESTINATION
+fi
+
+for db in `mysql -e 'show databases' | grep -Ev "$EXCLUDE"`; do 
+	echo -e "Dumping MySQL DB: $db"
+	mysqldump --events $db | gzip > $DESTINATION/$db.$CURDATE.sql.gz
+done
+
+#keep only last 24 hours backups
+find $DESTINATION -type f -mtime +1 -exec rm -f {} \;
