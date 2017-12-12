79b7140d9a971acd827dd60844d690c69f639c38
Stanislav Stoyanov <stanislav@bgcode.com>
2017-12-12 19:01:47 +0200

    1d sqlbackup plesk

---
+++ 1d-sqlbackups-plesk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MYSQL_PWD=`cat /etc/psa/.psa.shadow`
+EXCLUDE="'Database|Database|information\_schema|performance\_schema|Database'"
+DESTINATION=/root/sql/
+CURDATE=`date  +"%d-%m-%Y-%Hh%Mm"`
+
+
+if [ ! -d $DESTINATION ]; then
+	mkdir $DESTINATION
+fi
+
+for db in `mysql -u admin -p$MYSQL_PWD -e 'show databases' | grep -Ev "$EXCLUDE"`; do 
+	echo -e "Dumping MySQL DB: $db"
+	mysqldump -u admin -p$MYSQL_PWD --events $db | gzip > $DESTINATION/$db.$CURDATE.sql.gz
+done
+
+#keep only last 24 hours backups
+find $DESTINATION -type f -mtime +1 -exec rm -f {} \;
