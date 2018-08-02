2aae807914e95bdc67d758cdfec0dfb083d3e88d
Stanislav Stoyanov <stanislav@bgcode.com>
2018-08-02 13:28:09 +0300

    bcvpsd installed

--- bcvpsd/install.sh
+++ bcvpsd/install.sh
@@ -3,14 +3,14 @@
 if [ -f /etc/debian_version ]; then
     distro=debian
     apt update
-    apt install perl perl-base  -y
+    apt install perl perl-base libio-socket-ssl-perl  -y
+	
 fi
 
 if [ -f /etc/centos-release ];then
     distro=centos
-    yum install -y cpan perl-LWP-Protocol-https
+    yum install -y cpan perl-LWP-Protocol-https perl-IO-Socket-SSL
 fi
-
 
 export PERL_MM_USE_DEFAULT=1
 cpan install File::Pid Sys::Load Proc::ProcessTable LWP::UserAgent HTTP::Tiny
