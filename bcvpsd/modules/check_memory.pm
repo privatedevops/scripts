faac15e338128007b5317e10af117b41ec90aace
nampa <nampa@Stanislavs-Mac-mini.local>
2019-03-18 15:16:41 +0200

    test

--- bcvpsd/modules/check_memory.pm
+++ bcvpsd/modules/check_memory.pm
@@ -6,7 +6,6 @@ package check_memory;
 
 use Sys::MemInfo qw(totalmem freemem totalswap);
 
-
 use strict;
 use warnings;
 use base 'Exporter';
@@ -16,14 +15,18 @@ our @ISA        = qw(Exporter);
 our @EXPORT = qw(memavg);
 our $VERSION    = 1.0;
 
-our $freememory = (&freemem / 1024 /1024);
-
 __PACKAGE__->memavg() unless caller;
 
 sub memavg {
-	if ( $freememory < 200 ) {
-		print "Free mem is $freememory, restarting Nginx & PHP-FPM\n";
-		system ("/usr/local/sbin/9");
-		return 1;
-	}
+        my $freemem = (Sys::MemInfo::get("freemem") / 1024 / 1024 );
+
+        our $freememory = sprintf "%.0f", $freemem;
+
+        if ( $freememory < 2000 ) {
+                print "Free mem is $freememory, restarting Nginx & PHP-FPM\n";
+                system ("/usr/local/sbin/9");
+                return 1;
+        } else {
+                return 0;
+        }
 }
